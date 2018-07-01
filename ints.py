e4832256504a7f181b97b4a5656029263fb1f9f7
VaryAble <VaryAble@protonmail.ch>
2018-06-30 22:42:04 -0700

    Create ints.py

---
+++ ints.py
@@ -0,0 +1,19 @@
+# Integer arithmetic
+
+from cs50 import get_int
+
+# promt user for x
+x = get_int("x: ")
+
+# promt user for y
+y = get_int("y: ")
+
+# perform arithmetic
+print(f"{x} plus {y} is {x + y}")
+print(f"{x} minus {y} is {x - y}")
+print(f"{x} times {y} is {x * y}")
+print(f"{x} truly divided by {y} is {x / y}")
+print(f"{x} floor-diveded by {y} is {x // y}")
+print(f"remainder of {x} divided by {y} is {x % y}")
+
+# Thanks for looking
