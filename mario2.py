cc4870790b41a929303e15ee24c87ebc063b6dae
VaryAble <VaryAble@protonmail.ch>
2018-07-01 10:14:45 -0700

    Create mario2.py

---
+++ mario2.py
@@ -0,0 +1,10 @@
+# Prints any number of question marks, as specified by user
+
+from cs50 import get_int
+
+n = get_int("Number: ")
+for i in range(n):
+    print("?", end="")
+print()
+
+# Thanks for looking
