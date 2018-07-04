ad60edb0527aa3b6a474e6f14b80fc930cba6121
VaryAble <VaryAble@protonmail.ch>
2018-07-04 13:29:46 -0700

    Create evenOdd.py

---
+++ evenOdd.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wednesday July 4 1:26 2018
+
+@author: VaryAble
+"""
+
+x = int(input('Enter an integer: '))
+if x%2 == 0:
+    print('')
+    print('Even')
+else:
+    print('')
+    print('Odd')
+print('Done with condition')
+
+# Thanks for looking
